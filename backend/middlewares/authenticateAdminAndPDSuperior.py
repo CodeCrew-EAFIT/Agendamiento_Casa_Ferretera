bb053e67e9f9655404d2be52af169008da404b0c
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-28 16:30:58 -0500

    (update): Adapted authenticationAdmin middleware to accept P.D. Superior

--- backend/middlewares/authenticateAdminAndPDSuperior.py
+++ backend/middlewares/authenticateAdminAndPDSuperior.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException, Request
 
 # Middleware for admin authentication
 
-async def authenticateAdmin(request: Request):
+async def authenticateAdminAndPDSuperior(request: Request):
   user_type = request.headers.get("user-type")
-  if not user_type or user_type not in ["admin"]:
+  if not user_type or user_type not in ["admin", "promoters direct superior"]:
     raise HTTPException(status_code=403, detail="Forbidden Access")
