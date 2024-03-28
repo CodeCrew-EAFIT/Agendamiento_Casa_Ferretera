46c0eaa2ac842300d05934dd7cac3e3e61185791
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-28 16:28:46 -0500

    (delete): Delete authenticateSupervisor middleware file

--- backend/middlewares/authenticateSupervisor.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException, Request
-
-# Middleware for supervisor authentication
-
-async def authenticateSupervisor(request: Request):
-  user_type = request.headers.get("user-type")
-  if not user_type or user_type not in ["supervisor"]:
-    raise HTTPException(status_code=403, detail="Forbidden Access")
