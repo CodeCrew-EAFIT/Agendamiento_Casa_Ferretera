b069f7cf91ca874bb32f77167b829cf77c5b4a17
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-28 15:01:34 -0500

    (update): HTTPException detail in English

--- backend/middlewares/authenticateSupervisor.py
+++ backend/middlewares/authenticateSupervisor.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException, Request
 
+# Middleware for supervisor authentication
+
 async def authenticateSupervisor(request: Request):
   user_type = request.headers.get("user-type")
   if not user_type or user_type not in ["supervisor"]:
-    raise HTTPException(status_code=403, detail="Acceso denegado")
+    raise HTTPException(status_code=403, detail="Forbidden Access")
