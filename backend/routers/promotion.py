342e39ab9cba081d285f0ed0a08861ece48232f0
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-28 15:02:30 -0500

    (update): Create new route and modify existing one

--- backend/routers/promotion.py
+++ backend/routers/promotion.py
@@ -1,16 +1,27 @@
-from services.promotion import getPromotionsByPromoterId
+from services.promotion import *
 from fastapi import APIRouter, Depends
 from middlewares.authenticateUser import authenticateUser
 from schemas.promotion import Promotion
-from config.db import conn
 
 promotion = APIRouter()
 
-# Route to fetch promotions for a given promoter_user_id
-@promotion.get("/promotions/{promoter_user_id}")
-async def fetchPromotions(promoter_user_id: int): #, authenticated_user: None = Depends(authenticateUser)):
-    promotions = getPromotionsByPromoterId(promoter_user_id, conn)
+# Route to fetch a promotion given a promotion_id
+
+@promotion.get("/promotion/{promotion_id}")
+async def fetchPromotions(promotion_id: int): #, authenticated_user: None = Depends(authenticateUser)):
+    promotions = getPromotion(promotion_id)
     return promotions
+
+
+# Route to fetch promotions given a promoter_user_id
+
+@promotion.get("/promotions-by-promoter-id/{promoter_user_id}")
+async def fetchPromotions(promoter_user_id: int): #, authenticated_user: None = Depends(authenticateUser)):
+    promotions = getPromotionsByPromoterId(promoter_user_id)
+    return promotions
+
+
+# Test route
 
 @promotion.get("/")
 async def testFunction():
