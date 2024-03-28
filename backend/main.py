a037d7b7bca122736d20703368f984ca1ff91ced
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-28 00:37:05 -0500

    (update): Simplify main.py

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,9 @@
-from fastapi import FastAPI, HTTPException
-import libsql_experimental as libsql
-import uvicorn
+from fastapi import FastAPI
+from routers.promotion import promotion
 
 # Create FastAPI app
 app = FastAPI()
-
-# Function to fetch promotions for a given promoter_user_id
-def get_promotions(promoter_user_id):
-    conn = libsql.connect("acf.db")
-    query = "SELECT * FROM Promotion WHERE promoter_user_id = ?"
-    promotions = conn.execute(query, (promoter_user_id,)).fetchall()
-    return promotions
-
-# Route to fetch promotions for a given promoter_user_id
-@app.get("/promotions/{promoter_user_id}")
-async def fetch_promotions(promoter_user_id: int):
-    promotions = get_promotions(promoter_user_id)
-    print(promotions)
-    return promotions
+app.include_router(promotion)
 
 
 # Command to run the app:
