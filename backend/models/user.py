8022ab775cd660be634c98a0cef0f180c189c6bf
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-28 00:39:29 -0500

    (feature): Create each model

---
+++ backend/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Table, Column, Integer, Float, String, DateTime, ForeignKey, CheckConstraint, MetaData
+from config.db import meta, engine
+from .brand import brand
+
+user = Table(
+    'User',
+    meta,
+    Column('user_id', Integer, primary_key=True),
+    Column('name', String(20), nullable=False),
+    Column('password', String(20), nullable=False),
+    Column('email', String(25), nullable=False),
+    Column('phone_number', String(25), nullable=False),
+    Column('brand_id', Integer, ForeignKey('Brand.brand_id')),
+    Column('type', String, CheckConstraint('type IN ("admin", "promoter", "supervisor", "promoters direct superior")'), nullable=False),
+)
+
+meta.create_all(engine)
