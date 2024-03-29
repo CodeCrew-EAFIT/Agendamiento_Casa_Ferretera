97333786e51bffc92a4a7a10949c88e8a8da7f62
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-29 04:25:46 -0500

    (update): Change booking model

--- backend/models/booking.py
+++ backend/models/booking.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, CheckConstraint, MetaData
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, CheckConstraint, MetaData, Date, Time
 from config.db import meta, engine
 from .location import location
 from .user import user
@@ -6,10 +6,11 @@ from .user import user
 booking = Table(
     'Booking',
     meta,
-    Column('booking_id', Integer, primary_key=True),
+    Column('booking_id', Integer, primary_key=True, autoincrement=True),
     Column('location_id', Integer, ForeignKey('Location.location_id'), nullable=False),
-    Column('start_date', DateTime, nullable=False),
-    Column('end_date', DateTime, nullable=False),
+    Column('booking_date', Date, nullable=False),
+    Column('start_time', String(8), nullable=False),
+    Column('end_time', String(8), nullable=False),
     Column('created_at', DateTime, nullable=False),
     Column('user_id_created_by', Integer, ForeignKey('User.user_id'), nullable=False),
     Column('updated_at', DateTime),
