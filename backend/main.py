155ce0f2f32d70365d1cbcd0949ab9b5c4cc91c4
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-29 04:25:04 -0500

    (update): Add new router

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routers.promotion import promotion
+from routers.booking import booking
 
 # Create FastAPI app
 app = FastAPI()
 app.include_router(promotion)
+app.include_router(booking)
 
 
 # Command to run the app:
