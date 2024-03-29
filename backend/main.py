82a0416f56ac1728fe696a51c9585b53afe3e475
Julian David Valencia Restrepo <julianv302@gmail.com>
2024-03-29 14:42:55 -0500

    (Update) Update Main to create DataBase

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
-from routers.promotion import promotion
-
-# Create FastAPI app
-app = FastAPI()
-app.include_router(promotion)
-
-
 # Command to run the app:
 ## uvicorn main:app --host 127.0.0.1 --port 8080
+
+from fastapi import FastAPI
+from routers.auth import authRouter
+from config.db import Base, engine
+from models import *
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+app.include_router(authRouter)
