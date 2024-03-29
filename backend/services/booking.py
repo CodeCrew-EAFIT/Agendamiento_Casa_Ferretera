07d0c4aab7ca3ac3ae9b57bc7b5482bd44a92dd7
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-29 17:44:43 -0500

    (update): Add check availability function for promotion scheduling

--- backend/services/booking.py
+++ backend/services/booking.py
@@ -1,16 +1,17 @@
 from fastapi import HTTPException
 from sqlalchemy import select, column, exists
 from services.convertToDictionary import convertToDictionary
-from models.booking import booking as booking_table
+from models.booking import booking as bookingTable
 from schemas.booking import Booking
 from config.db import conn
+from datetime import date, datetime
 
 
 # Function to create a booking given a Booking object
 
 def createBooking(booking: Booking):
 
-    booking_dict = {
+    bookingDict = {
         "location_id": booking.location_id,
         "booking_date": booking.booking_date,
         "start_time": booking.start_time,
@@ -22,18 +23,52 @@ def createBooking(booking: Booking):
         "change_reason": booking.change_reason
     }
     
-    result = conn.execute(booking_table.insert().values(booking_dict))
+    result = conn.execute(bookingTable.insert().values(bookingDict))
     conn.commit()
     return result
 
 
 # Function to fetch a booking given a booking_id
 
-def getBooking(booking_id):
-    query = select(booking_table).where(booking_table.c.booking_id == booking_id)
+def getBooking(bookingId: int):
+    query = select(bookingTable).where(bookingTable.c.booking_id == bookingId)
     promotion = conn.execute(query).first()
     if promotion is not None:
         results = convertToDictionary(promotion)
         return results
     else:
         raise HTTPException(status_code=404, detail="Not Found")
+    
+
+# Function to check the availabily of a location given the date, start time and end time
+    
+def checkAvailability(date: date, startTime2: str, endTime2: str, locationId: int):
+    query = select(bookingTable).where(bookingTable.c.booking_date == date, bookingTable.c.location_id == locationId)
+    bookings = conn.execute(query).fetchall()
+    bookingsDict = convertToDictionary(bookings)
+    available = True
+
+    for booking in bookingsDict:
+        startTime = booking['start_time']
+        endTime = booking['end_time']
+        print(f'{startTime} - {endTime}')
+
+        startTimeObj = datetime.strptime(startTime, "%H:%M:%S")
+        startTimeObj2  = datetime.strptime(startTime2, "%H:%M:%S")
+        hourDiff = startTimeObj.hour - startTimeObj2.hour
+        minuteDiff = startTimeObj.minute - startTimeObj2.minute
+        secondDiff = startTimeObj.second - startTimeObj2.second
+        print(f'{hourDiff} | {minuteDiff} | {secondDiff}')
+
+        if hourDiff <= 0:
+            print('La hora programada es después de la hora ya establecida')
+        elif minuteDiff <=0: 
+            print('La hora programada es después de la hora ya establecida')
+        elif secondDiff <= 0:
+            print('La hora programada es después de la hora ya establecida')
+        else: 
+            print('La hora programada es antes que la establecida')
+
+    print(bookingsDict[0]['booking_date'])
+
+    return bookings
