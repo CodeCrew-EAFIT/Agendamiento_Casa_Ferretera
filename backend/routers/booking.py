a8129bedf3c629a7d0a14923d4e864200af7c2f3
EsteTruji <estebantrujillocarmona@gmail.com>
2024-03-29 04:29:03 -0500

    (feature): Create new routes file for booking's

---
+++ backend/routers/booking.py
@@ -0,0 +1,13 @@
+from services.promotion import *
+from services.booking import *
+from fastapi import APIRouter, Depends
+from middlewares.authenticateUser import authenticateUser
+
+booking = APIRouter()
+
+# Route to fetch a booking given a booking_id
+
+@booking.get("/booking/{booking_id}")
+async def fetchBooking(booking_id: int): #, authenticated_user: None = Depends(authenticateUser)):
+    promotions = getBooking(booking_id)
+    return promotions
