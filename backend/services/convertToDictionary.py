67c080cc1768852b2b8f3b66cc38a522694c456e
EsteTruji <estebantrujillocarmona@gmail.com>
2024-04-01 16:22:53 -0500

    (delete): Delete file for it's not used

--- backend/services/convertToDictionary.py
+++
@@ -1,8 +0,0 @@
-
-def convertToDictionary(response):
-    listOfDictionaries = []
-    if type(response) is not list:
-        response = [response]
-    for object in response:
-        listOfDictionaries.append(object._asdict())
-    return listOfDictionaries
