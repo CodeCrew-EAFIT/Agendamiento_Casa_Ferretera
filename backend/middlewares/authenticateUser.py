e2173f72d6894aa3cb6464740b68e60392e1febf
Damian <damianduquel@gmail.com>
2024-03-31 20:21:38 -0500

    (delete): Unnecessary middlewares
    Co-authored-by: Esteban Trujillo <EsteTruji@users.noreply.github.com>

--- backend/middlewares/authenticateUser.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException, Request
-
-# Middleware for user authentication - needs to have one the listed user types for accessing
-
-async def authenticateUser(request: Request):
-  user_type = request.headers.get("user-type")
-  if not user_type or user_type not in ["admin", "promoter", "supervisor", "promoters direct superior"]:
-    raise HTTPException(status_code=403, detail="Forbidden Access")
