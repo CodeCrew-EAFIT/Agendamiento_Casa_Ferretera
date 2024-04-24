93795889a661ec3fe1eee69c9c25e4cd7f5fdc95
EsteTruji <estebantrujillocarmona@gmail.com>
2024-04-23 22:38:26 -0500

    (update): Add JWT to backend endpoints
    Co-authored-by: DamianDuque <DamianDuque@users.noreply.github.com>
    Co-authored-by: Julian David Valencia Restrepo <jdvalencir@users.noreply.github.com>
    Co-authored-by: Esteban Trujillo <EsteTruji@users.noreply.github.com>

--- backend/routers/booking.py
+++ backend/routers/booking.py
@@ -1,19 +1,20 @@
 from services.promotion import *
 from services.booking import *
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
 from services.getUserRole import *
+from utils import token, security
 
 bookingRouter = APIRouter()
 
 # Route to fetch all promotions
-@bookingRouter.get("/all-bookings")
+@bookingRouter.get("/all-bookings", dependencies=[Depends(token.JWTBearer())])
 async def fetchAllBookings(): #, authenticated_user: None = Depends(authenticateUser)):
     allBookings = getAllBookings()
     return allBookings
 
 
 # Route to fetch a booking given a booking_id
-@bookingRouter.get("/booking/{booking_id}")
+@bookingRouter.get("/booking/{booking_id}", dependencies=[Depends(token.JWTBearer())])
 async def fetchBooking(booking_id: int): #, authenticated_user: None = Depends(authenticateUser)):
     bookings = getBooking(booking_id)
     return bookings
--- backend/routers/reports.py
+++ backend/routers/reports.py
@@ -1,16 +1,19 @@
 from services.getUserRole import getUserRole
 from services.promotion import *
 from services.reports import *
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from middlewares.getIdFromHeader import getIdFromHeader
 from schemas.reports import Reports
+from utils import token, security
 
 reportsRouter = APIRouter()
 
 
-@reportsRouter.get("/reports-info")
-async def fetchGeneralInfo(authenticated_user: None = Depends(getIdFromHeader)):
-    userRole = getUserRole(authenticated_user)
+@reportsRouter.get("/reports-info",  dependencies=[Depends(token.JWTBearer())])
+async def fetchGeneralInfo(request: Request):
+    authorizationToken = request.headers.get('Authorization').split(' ')[1]
+    payload = token.decodeToken(authorizationToken)
+    userRole = getUserRole(payload["id"])
     if userRole == "supervisor" or userRole == "administrador":
         ginfo = preReportInfo()
         return ginfo
@@ -18,11 +21,13 @@ async def fetchGeneralInfo(authenticated_user: None = Depends(getIdFromHeader)):
         raise HTTPException(status_code=403, detail="Forbidden Access")
 
 
-@reportsRouter.post("/create-report")
-async def createReport(request:Reports, authenticated_user: None = Depends(getIdFromHeader)):
-    userRole = getUserRole(authenticated_user)
+@reportsRouter.post("/create-report",  dependencies=[Depends(token.JWTBearer())])
+async def createReport(reports: Reports, request: Request):
+    authorizationToken = request.headers.get('Authorization').split(' ')[1]
+    payload = token.decodeToken(authorizationToken)
+    userRole = getUserRole(payload["id"])
     if userRole == "supervisor" or userRole == "administrador":
-        ginfo = reportGenerator(request)
+        ginfo = reportGenerator(reports)
         return {"Report data:":ginfo}
     else:
         raise HTTPException(status_code=403, detail="Forbidden Access")
