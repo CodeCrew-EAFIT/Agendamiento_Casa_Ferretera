1873c0bb1d2ba7658849bf442fe2c7817e374c06
Hobar <huparelaa@eafit.edu.co>
2024-04-29 16:42:23 -0500

    (Test): Add tests using JWT permissions

---
+++ backend/tests/constants.py
@@ -0,0 +1,8 @@
+ADMIN_USER_DATA = {
+                "name": "string",
+                "role": "administrador",
+                "email": "admin@gmail.com",
+                "password": "admin",
+                "phone_number": "1234567890",
+                "brand_id": 10000,
+            }
