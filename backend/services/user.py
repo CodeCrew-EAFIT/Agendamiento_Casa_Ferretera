56a167eb61f53a689635e063aa69c4b3d77877fc
Damian <damianduquel@gmail.com>
2024-05-22 04:06:32 -0500

    (feature): upload/download documents

--- backend/services/user.py
+++ backend/services/user.py
@@ -5,6 +5,10 @@ from services.brand import *
 from models.brand import Brand
 from config.db import get_db
 from sqlalchemy.orm import defer
+from google.cloud import storage
+import os
+import base64
+import shutil
 
 
 
@@ -67,3 +71,79 @@ def getUserById(userId: int):
     else:
         raise HTTPException(status_code=404, detail="No encontrado")
     
+
+
+def getDocumentsByUser(userId: int):
+    try:
+            
+        storage_client = storage.Client()
+        bucket =storage_client.get_bucket("casa_ferre")
+        blob = bucket.blob(str(userId)+"/cedula.pdf")
+        blob2 = bucket.blob(str(userId)+"/seguridad-social.pdf")
+        
+        tmp_folder = os.path.join(os.getcwd(),"tempfolder")
+        os.mkdir(tmp_folder)
+        
+        with open(os.path.join(tmp_folder,'cedula.pdf'), 'wb') as f:
+            storage_client.download_blob_to_file(blob,f)
+        
+        with open(os.path.join(tmp_folder,'seguridad-social.pdf'), 'wb') as f:
+            storage_client.download_blob_to_file(blob,f)
+
+        with open(os.path.join(tmp_folder,'cedula.pdf'), "rb") as file:
+            cedula_string = base64.b64encode(file.read()).decode("utf-8")
+
+        with open(os.path.join(tmp_folder,'seguridad-social.pdf'), "rb") as file:
+            seguridad_social_string = base64.b64encode(file.read()).decode("utf-8")
+        
+        shutil.rmtree(tmp_folder)
+        return {"cc":cedula_string, "ss":seguridad_social_string}
+    
+    except Exception as e:
+        return e
+
+
+def upload_cedulac(userId: int, document: str):
+    try:
+        
+        storage_client = storage.Client()
+        bucket =storage_client.get_bucket("casa_ferre")
+        blob = bucket.blob(str(userId)+"/cedula.pdf")
+
+
+        tmp_folder = os.path.join(os.getcwd(),"tempfolder")
+        print(tmp_folder)
+        os.mkdir(tmp_folder)
+
+        with open(os.path.join(tmp_folder,'cedula.pdf'), 'wb') as file:
+            file.write(base64.b64decode(document))
+
+
+        blob.upload_from_filename(os.path.join(tmp_folder,"cedula.pdf"))
+        shutil.rmtree(tmp_folder)
+        return {'message': 'Documento subido exitosamente.'}
+    
+    except Exception as e:
+        return e
+
+
+def upload_seguridad_social(userId: int, document: str):
+    try:
+        storage_client = storage.Client()
+        bucket =storage_client.get_bucket("casa_ferre")
+        blob = bucket.blob(str(userId)+"/seguridad-social.pdf")
+
+
+        tmp_folder = os.path.join(os.getcwd(),"tempfolder")
+        os.mkdir(tmp_folder)
+
+        with open(os.path.join(tmp_folder,'seguridad-social.pdf'), 'wb') as file:
+            file.write(base64.b64decode(document))
+
+
+        blob.upload_from_filename(os.path.join(tmp_folder,"seguridad-social.pdf"))
+        shutil.rmtree(tmp_folder)
+        return {'message': 'Documento subido exitosamente.'}
+    
+    except Exception as e:
+        return e
