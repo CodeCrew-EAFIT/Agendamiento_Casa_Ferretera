5c7a88d38c4cb4b1187f7291773493b35aeba640
Castro-1 <jecastrog@eafit.edu.co>
2024-05-22 13:01:01 -0500

    (feature): Edit promotion done

--- frontend/src/pages/EditPromotion.jsx
+++ frontend/src/pages/EditPromotion.jsx
@@ -6,14 +6,15 @@ import { useLocationContext } from '../utils/LocationContext'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { useUserSession } from '../utils/UserSessionContext'
+import { AVAILABLE_HOURS_MILITARY_NORMAL } from '../utils/constants'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export default function EditPromotion () {
   const { id } = useParams()
   const { handleLogout } = useUserSession()
-
   const { location } = useLocationContext()
+  const [promotion, setPromotion] = useState(null)
   const [formData, setFormData] = useState({
     location,
     date: '',
@@ -23,14 +24,51 @@ export default function EditPromotion () {
     comments: ''
   })
 
-  const fetchPromotion = async () => {
+  const fetchBooking = async (bookingId) => {
     try {
-      const result = await axios.get(`${BASE_URL}/booking/${id}`, {
+      const result = await axios.get(`${BASE_URL}/booking/${bookingId}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
       })
       console.log(result.data)
+      setFormData((prev) => ({
+        ...prev,
+        date: result.data?.booking_date,
+        startTime: AVAILABLE_HOURS_MILITARY_NORMAL[result.data?.start_time],
+        endTime: AVAILABLE_HOURS_MILITARY_NORMAL[result.data?.end_time]
+      }))
+    } catch (error) {
+      if (error.response.status === 403) {
+        handleLogout()
+      }
+    }
+  }
+
+  const fetchPromotion = async () => {
+    try {
+      const result = await axios.get(`${BASE_URL}/promotion/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      console.log(result.data)
+      setPromotion(result.data)
+    } catch (error) {
+      if (error.response.status === 403) {
+        handleLogout()
+      }
+    }
+  }
+
+  const fetchPromoter = async (promoterId) => {
+    try {
+      const result = await axios.get(`${BASE_URL}/user-by-id/${promoterId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      setFormData((prev) => ({ ...prev, promoter: result.data.name }))
     } catch (error) {
       if (error.response.status === 403) {
         handleLogout()
@@ -42,9 +80,16 @@ export default function EditPromotion () {
     fetchPromotion()
   }, [])
 
+  useEffect(() => {
+    if (promotion && promotion.promoter_user_id){
+      fetchBooking(promotion.booking_id)
+      fetchPromoter(promotion.promoter_user_id)
+    }
+  }, [promotion])
+
   return (
     <Layout>
-      <div className="schedule-container">
+      {formData.endTime && formData.promoter && <div className="schedule-container">
         <div className="col-span-2 schedule-item">
           <Form
             formData={formData}
@@ -55,7 +100,7 @@ export default function EditPromotion () {
         <div className="col-span-1 schedule-item">
           <Calendar formData={formData} />
         </div>
-      </div>
+      </div>}
     </Layout>
   )
 }
