08545251229525e9c6839326aa94831e639cc428
Julián David Valencia <julianv302@gmail.com>
2025-02-02 14:55:13 -0500

    fix: fix connection to turso db

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "libsql://casaferreteradb-jdvalencir.turso.io"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={ 'check_same_thread': False}, pool_size=20, max_overflow=30)
+TURSO_DATABASE_URL = os.environ.get("DATABASE_URL")
+TURSO_AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+
+dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+
+engine = create_engine(dbUrl, connect_args={'check_same_thread': False}, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
